947e37894734b6b6582df42a5a0f92657d7d866c
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-03-23 13:34:39 +0600

    data added on user dashboard home

--- src/Dashboard/RegularUser/RegularUserHome/RegularUserHome.jsx
+++ src/Dashboard/RegularUser/RegularUserHome/RegularUserHome.jsx
@@ -1,5 +1,29 @@
+import StatChart from "../../SupportComponents/StatChart";
+import StatusCard from "../../SupportComponents/StatusCard";
+import Table from "../../SupportComponents/Table";
+
 const RegularUserHome = () => {
-  return <div>This is user home</div>;
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <StatusCard title="My Course" value="0" />
+        <StatusCard title="My Exam" value="0" />
+        <StatusCard title="Blog Read" value="0" />
+      </div>
+
+      {/* Chart Section */}
+      <div className="mt-6">
+        <h2 className="text-xl font-bold mb-3">Growth (Exam)</h2>
+        <StatChart />
+      </div>
+
+      {/* Table */}
+      <div className="mt-6">
+        <h2 className="text-xl font-bold mb-3">Recent Purchase</h2>
+        <Table />
+      </div>
+    </div>
+  );
 };
 
 export default RegularUserHome;
