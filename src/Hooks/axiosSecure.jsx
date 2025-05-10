48e6480fed9506f3c4adbff0c6634d8c6b07dfc9
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-05-10 23:23:19 +0600

    remove jwt token for a while

--- src/Hooks/axiosSecure.jsx
+++ src/Hooks/axiosSecure.jsx
@@ -4,40 +4,40 @@ import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
   // local api
-  // baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5000",
   // live api
-  baseURL: "https://assessly-server.vercel.app",
+  // baseURL: "https://assessly-server.vercel.app",
 });
 
 const useAxiosSecure = () => {
-  const navigate = useNavigate();
-  const { logout } = useAuth();
-  axiosSecure.interceptors.request.use(
-    function (config) {
-      const token = localStorage.getItem("token");
-      console.log("request stopped by interceptors", token);
-      config.headers.authorization = `Bearer ${token}`;
-      return config;
-    },
-    function (error) {
-      return Promise.reject(error);
-    }
-  );
+  // const navigate = useNavigate();
+  // const { logout } = useAuth();
+  // axiosSecure.interceptors.request.use(
+  //   function (config) {
+  //     const token = localStorage.getItem("token");
+  //     console.log("request stopped by interceptors", token);
+  //     config.headers.authorization = `Bearer ${token}`;
+  //     return config;
+  //   },
+  //   function (error) {
+  //     return Promise.reject(error);
+  //   }
+  // );
 
   // Add a response interceptor
-  axiosSecure.interceptors.response.use(
-    function (response) {
-      return response;
-    },
-    async (error) => {
-      const status = error.response.status;
-      if (status === 401 || status === 403) {
-        await logout();
-        navigate("/login", { replace: true });
-      }
-      return Promise.reject(error);
-    }
-  );
+  // axiosSecure.interceptors.response.use(
+  //   function (response) {
+  //     return response;
+  //   },
+  //   async (error) => {
+  //     const status = error.response.status;
+  //     if (status === 401 || status === 403) {
+  //       await logout();
+  //       navigate("/login", { replace: true });
+  //     }
+  //     return Promise.reject(error);
+  //   }
+  // );
   return axiosSecure;
 };
 
