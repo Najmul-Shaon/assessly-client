ff68c14e4e2ae7e061eeea9b64fe981e361dd660
Najmul shaon <najmul.nh.shaon@gmail.com>
2025-05-20 00:30:52 +0600

    added user dashboard data

--- src/Dashboard/RegularUser/RegularUserHome/RegularUserHome.jsx
+++ src/Dashboard/RegularUser/RegularUserHome/RegularUserHome.jsx
@@ -1,14 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../Hooks/axiosSecure";
 import StatChart from "../../SupportComponents/StatChart";
 import StatusCard from "../../SupportComponents/StatusCard";
 import Table from "../../SupportComponents/Table";
+import useAuth from "../../../Hooks/useAuth";
 
 const RegularUserHome = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: userDashboardStats = [] } = useQuery({
+    queryKey: ["userDashboardStats", user?.email],
+    enabled: !!user?.email && !!localStorage.getItem("token"),
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/get/dashboard-stats/regular-user?user=${user?.email}`
+      );
+      return res.data;
+    },
+  });
+  console.log(userDashboardStats);
   return (
     <div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <StatusCard title="My Course" value="0" />
-        <StatusCard title="My Exam" value="0" />
-        <StatusCard title="Blog Read" value="0" />
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
+        <StatusCard
+          title="My Course"
+          value={userDashboardStats?.enrolledCoursesCount}
+        />
+        <StatusCard
+          title="My Exam"
+          value={userDashboardStats?.enrolledExamsCount}
+        />
+        <StatusCard
+          title="Blog Read"
+          value={userDashboardStats?.readBlogCount}
+        />
+        <StatusCard
+          title="Total Paid"
+          value={userDashboardStats?.totalPaidAmount}
+        />
       </div>
 
       {/* Chart Section */}
